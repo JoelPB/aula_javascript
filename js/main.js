49cd9b7aa30e3aafc0ab510cbad87605864145bc
JoelPB <Joel.mail.joel@gmail.com>
2020-07-07 17:28:22 -0300

    m2
    Signed-off-by: JoelPB <Joel.mail.joel@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 
 
-var idade = 18
+var count = 0
+while(count < 5){
+    console.log(count)
+    count = count + 1
+}
+/*var idade = prompt('Qual a sua idade?')
+//var idade = 18
 
 if(idade >= 18){
     alert('Maior de idade')
 }else{
     alert('Menor de idade')
-}
+}*/
 
 /*var frutas = [{nome:'maçã', cor:'vermelha'}, {nome:'uva', cor:'roxa'}]
 console.log(frutas)
